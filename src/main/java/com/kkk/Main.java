bc8e3988941592eec0234661a07334d966ab89ad
Cunninger <1803912219@qq.com>
2024-07-04 23:11:56 +0800

    hello test1

--- src/main/java/com/kkk/Main.java
+++ src/main/java/com/kkk/Main.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
+    // hello
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
